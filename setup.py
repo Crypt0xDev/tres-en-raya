09946021d99e1dd5163efd49185088ac8242de52
Crypt0xDev <alexisgonzalesperezuniversidad@gmail.com>
2025-09-28 16:07:05 -0500

    🔧 Corregir configuración GitHub Actions
    ✅ Correcciones aplicadas:
    • Actualizar setup-python de v4 a v5
    • Especificar versiones Python con comillas en YAML
    • Añadir python_requires='>=3.9' en setup.py
    • Especificar versiones Python soportadas (3.9-3.12)
    
    🐛 Soluciona:
    • Error "Version 3.1 was not found" en GitHub Actions
    • Mejora compatibilidad y especificación de versiones
    • Asegura funcionamiento correcto del CI/CD

--- setup.py
+++ setup.py
@@ -23,9 +23,15 @@ setup(
     author='Crypt0xDev',
     author_email='alexis.alvarado@unsm.edu.pe',
     url='https://github.com/Crypt0xDev/tres-en-raya',
+    python_requires='>=3.9',
     classifiers=[
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Framework :: Flask',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
